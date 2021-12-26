b12466ebc00e7e5573e7c0b31ca6ec39771f7cb3
viswayadeedya <viswayadeedya@gmail.com>
2021-12-26 19:42:05 +0530

    created new module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { CommonModule } from "@angular/common";
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { NgxBootstrapIconsModule, allIcons } from "ngx-bootstrap-icons";
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
+import { BodyModule } from './body/body.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,6 +21,8 @@ import { NgxBootstrapIconsModule, allIcons } from "ngx-bootstrap-icons";
     BrowserAnimationsModule,
     ReactiveFormsModule,
     FormsModule,
+    BodyModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
