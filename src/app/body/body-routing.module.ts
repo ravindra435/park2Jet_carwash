0c02b8aa33828aa8559b97fed9792a3ee87a8b93
ravindrakumar435 <92420867+ravindrakumar435@users.noreply.github.com>
2021-12-28 00:11:38 +0530

    communitypage design completed

--- src/app/body/body-routing.module.ts
+++ src/app/body/body-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommunityComponent } from './community/community.component';
 import { ContactComponent } from './contact/contact.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { GiftCardsComponent } from './gift-cards/gift-cards.component';
+import { JoinTeamComponent } from './join-team/join-team.component';
 import { MaindashboardComponent } from './maindashboard/maindashboard.component';
+import { ShopComponent } from './shop/shop.component';
+import { WashClubComponent } from './wash-club/wash-club.component';
 
 const routes: Routes = [
   {
@@ -12,6 +17,11 @@ const routes: Routes = [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', component: DashboardComponent },
       { path: 'contact', component: ContactComponent },
+      { path: 'community', component: CommunityComponent },
+      { path: 'join-the-team', component: JoinTeamComponent },
+      { path: 'gift-cards', component: GiftCardsComponent },
+      { path: 'shop', component: ShopComponent },
+      { path: 'unlimited', component: WashClubComponent },
     ],
   },
 ];
